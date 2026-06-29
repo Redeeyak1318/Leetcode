9d50bcd5f5b9044779b8e58a38049fb9b00b4e5e
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-06-29 22:26:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.77%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,14 +4,14 @@ public:
         int rev = 0;
 
         while (x != 0) {
-
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && x%10 > 7))
+            int digit = x%10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
                 return 0;
 
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && x%10 < -8))
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
                 return 0;
 
-            rev = rev * 10 + (x%10);
+            rev = rev * 10 + digit;
             x /= 10;
         }
 
