3c7edc7b3846564c5d8bccec7bdf063922e87405
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-06-30 01:21:52 +0530

    Time: 0 ms (100%), Space: 10.4 MB (73.38%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,44 @@
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int *ans = (int *)malloc(2 * sizeof(*ans));
+    ans[0]=-1;
+    ans[1]=-1;
+    
+    int low=0;
+    int high=numsSize-1;
+    while(low<=high)
+    {
+        int mid=low + (high-low)/2;
+        if(nums[mid]==target)
+        {
+            ans[0]=mid;
+            high=mid-1;
+        }
+        else if(nums[mid]<target)
+        {
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    low=0;
+    high=numsSize-1;
+    while(low<=high)
+    {
+        int mid=low + (high-low)/2;
+        if(nums[mid]==target)
+        {
+            ans[1]=mid;
+            low=mid+1;;
+        }
+        else if(nums[mid]<target)
+        {
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    *returnSize = 2;
+    return ans;
+}
