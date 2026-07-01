5748e723b1554b15edf0f4ed8343bd2c5c40dd3e
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-01 18:07:08 +0530

    Time: 21 ms (49.41%), Space: 16.2 MB (62.35%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.c
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.c
@@ -1,3 +1,25 @@
+bool bloom(int* bloomDay, int bloomDaySize, int day, int m, int k)
+{
+    int flowCount=0;
+    int bouquets=0;
+    for(int i=0;i<bloomDaySize;i++)
+    {
+        if(bloomDay[i]<=day)
+        {
+            flowCount++;
+        }
+        else
+        {
+            flowCount=0;
+        }
+        if(flowCount==k)
+        {
+            bouquets++;
+            flowCount=0;
+        }
+    }
+    return bouquets>=m;
+}
 int minDays(int* bloomDay, int bloomDaySize, int m, int k) {
     if((long long)m * k >bloomDaySize)
         return -1;
@@ -14,25 +36,7 @@ int minDays(int* bloomDay, int bloomDaySize, int m, int k) {
     while(low<high)
     {
         int mid = low + (high-low)/2;
-        int flowCount=0;
-        int bouquets=0;
-        for(int i=0;i<bloomDaySize;i++)
-        {
-            if(bloomDay[i]<=mid)
-            {
-                flowCount++;
-            }
-            else
-            {
-                flowCount=0;
-            }
-            if(flowCount==k)
-            {
-                bouquets++;
-                flowCount=0;
-            }
-        }
-        if(bouquets>=m)
+        if(bloom(bloomDay, bloomDaySize, mid, m, k))
             high=mid;
         else
             low=mid+1;
