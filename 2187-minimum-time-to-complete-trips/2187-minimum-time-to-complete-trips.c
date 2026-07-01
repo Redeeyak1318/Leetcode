2ec456eefaccc00eef82d49a8f549f5bceb3f16e
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-02 01:13:22 +0530

    Time: 34 ms (97.67%), Space: 17.4 MB (31.4%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.c
@@ -0,0 +1,32 @@
+bool canTime(int* time, int timeSize, long long currTime, int totalTrips)
+{
+    long long trips = 0;
+    for(int i=0; i<timeSize; i++)
+    {
+        trips += currTime/time[i];
+
+        if(trips>=totalTrips)
+                return true;
+    }
+    return false;    
+}
+
+long long minimumTime(int* time, int timeSize, int totalTrips) {
+    long long low = time[0];
+    long long high = low * totalTrips;
+    for(int i=1; i<timeSize; i++)
+    {
+        if(time[i]<low)
+            low = time[i];
+    }
+
+    while(low < high)
+    {
+        long long mid = low + (high - low)/2;
+        if(canTime(time, timeSize, mid, totalTrips))
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
