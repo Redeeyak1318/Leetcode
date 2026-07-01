39ab93c369cc13f66a1abe022dc56481994719bb
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-01 23:49:29 +0530

    Time: 0 ms (100%), Space: 9.3 MB (80.91%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.c
@@ -0,0 +1,25 @@
+bool totalCoins(int n, int k)
+{
+    if((long long)k*(k+1)/2 <= n)
+    {
+        return true;
+    }
+    return false;
+}
+
+int arrangeCoins(int n) {
+    int low=0;
+    int high=n;
+    while(low<=high)
+    {
+        int mid = low + (high-low)/2;
+        if(totalCoins(n, mid))
+        {
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return high;
+}
