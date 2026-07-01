59323201c24a8534d01c7c47d74e358992e03a60
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-02 02:07:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -1,22 +1,21 @@
 int searchInsert(int* nums, int numsSize, int target) {
-    int low=0;
-    int high=numsSize-1;
-    int ans=numsSize;
-    while(low<=high)
+    if(target < nums[0])
+        return 0;
+    int low = 0;
+    int high = numsSize - 1;
+    int ans = numsSize;
+    while(low <= high)
     {
-        int mid=low + (high-low)/2;
-        if(nums[mid]==target)
-        {
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target)
             return mid;
-        }
-        else if(nums[mid]>target)
+        else if(nums[mid] > target)
         {
-            ans=mid;
-            high=mid-1;
+            ans = mid;
+            high = mid -1;
         }
-        else{
-            low=mid+1;
-        }
+        else
+            low = mid + 1;
     }
     return ans;
 }
