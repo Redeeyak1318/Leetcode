e4508a5745deee36cd8a104c186933346f56b202
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-01 14:44:57 +0530

    Time: 10 ms (65.3%), Space: 11.8 MB (9.77%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.c
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.c
@@ -1,3 +1,27 @@
+bool canship(int *weights, int weightsSize, int capacity, int days)
+{
+    int daysneeded=1;
+    int currload=0;
+    for(int i=0;i<weightsSize;i++)
+    {
+        if(currload+weights[i]<=capacity)
+        {
+            currload+=weights[i];
+        }
+        else
+        {
+            daysneeded++;
+            currload=weights[i];
+
+            if(daysneeded>days)
+            {
+                break;
+            }
+        }
+    }
+    return daysneeded<=days;
+}
+
 int shipWithinDays(int* weights, int weightsSize, int days) {
     int low=weights[0];
     int high=weights[0];
@@ -10,30 +34,11 @@ int shipWithinDays(int* weights, int weightsSize, int days) {
     while(low<high)
     {
         int mid=low + (high-low)/2;
-        int daysneeded=1;
-        int currload=0;
-        for(int i=0;i<weightsSize;i++)
-        {
-            if(currload+weights[i]<=mid)
-            {
-                currload+=weights[i];
-            }
-            else
-            {
-                daysneeded++;
-                currload=weights[i];
-
-                if(daysneeded>days)
-                {
-                    break;
-                }
-            }
-        }
-        if(daysneeded<=days)
+        
+        if(canship(weights, weightsSize, mid, days))
             high=mid;
         else
             low=mid+1;
     }
-    return low;
-    
+    return low;  
 }
