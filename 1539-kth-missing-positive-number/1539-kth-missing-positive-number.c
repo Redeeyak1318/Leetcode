d51e77792e0b733d54018622f689e16cd3dd88ed
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-03 01:17:58 +0530

    Time: 0 ms (100%), Space: 9 MB (99.4%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.c
@@ -0,0 +1,25 @@
+bool isPossible(int *arr, int arrSize, int k, int i)
+{
+    if(arr[i] - i - 1 >= k)
+        return true;
+    return false;
+}
+int findKthPositive(int* arr, int arrSize, int k) {
+    int low = 0;
+    int high = arrSize - 1;
+    int index;
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+        if(isPossible(arr, arrSize, k, mid))
+        {
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    index = low;
+    int ans = index + k;
+    return ans;
+}
