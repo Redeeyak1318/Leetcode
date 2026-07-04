78382ae4ae353ff31ba24c48d245299931b8e69e
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-04 21:49:04 +0530

    Time: 0 ms (100%), Space: 13.1 MB (82.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.c
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.c
@@ -1,28 +1,23 @@
 int singleNonDuplicate(int* nums, int numsSize) {
-    int low=0;
-    int high=numsSize-1;
-    while(low<high)
+    int low = 0;
+    int high = numsSize - 1;
+    while(low < high)
     {
-        int mid=low + (high-low)/2;
-        if(mid%2!=0)
+        int mid = low + (high - low)/2;
+        if(mid % 2 == 0)
         {
-            if(nums[mid]==nums[mid-1])
-            {
-                low=mid+1;
-            }
-            else{
-                high=mid;
-            }
+            if(nums[mid] == nums[mid+1])
+                low = mid + 1;
+            else
+                high = mid;
         }
         else{
-            if(nums[mid]==nums[mid+1])
-            {
-                low=mid+1;
-            }
-            else{
-                high=mid;
-            }
+            if(nums[mid] == nums[mid-1])
+                low = mid + 1;
+            else
+                high = mid;
         }
     }
     return nums[low];
 }
+
