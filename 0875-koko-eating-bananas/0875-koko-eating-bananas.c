ccdd8850e5fd984547e690b53328e6903f847f25
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-12 20:18:21 +0530

    Time: 7 ms (59.13%), Space: 10 MB (23.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.c
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.c
@@ -1,26 +1,25 @@
 int minEatingSpeed(int* piles, int pilesSize, int h) {
-    int low=1;
-    int high=piles[0];
-    for(int i=1;i<pilesSize;i++)
+    int low = 1;
+    int high = piles[0];
+    for(int i=1; i<pilesSize; i++)
     {
-        if(piles[i]>high)
-            high=piles[i];
+        if(piles[i] > high)
+            high = piles[i];
     }
-    while(low<high)
+    while(low < high)
     {
-        int mid=low + (high-low)/2;
-        int TotalHrs=0;
-        for(int i = 0; i < pilesSize; i++)
+        int mid = low + (high - low)/2;
+        int TotalHrs = 0;
+        for(int i=0; i<pilesSize; i++)
         {
-            TotalHrs +=(piles[i] + mid - 1) / mid;
-
+            TotalHrs += (piles[i] + mid - 1)/mid;
             if(TotalHrs > h)
                 break;
         }
-        if(TotalHrs<=h)
-            high=mid;
+        if(TotalHrs <= h)
+            high = mid;
         else
-            low=mid+1;
+            low = mid + 1;
     }
     return low;
 }
