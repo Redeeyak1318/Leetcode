fa35e3b074456d3cbc64701bc5cec9d4746b39fc
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-14 00:20:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (51.67%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.c
@@ -0,0 +1,35 @@
+int splitArray(int* nums, int numsSize, int k) {
+    int low = nums[0];
+    int high = nums[0];
+    for(int i=1; i<numsSize; i++)
+    {
+        if(nums[i] > low)
+            low = nums[i];
+        high += nums[i];
+    }
+
+    int minSum = 0;
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+        int currsum = 0;
+        int subArray = 1;
+        for(int i=0; i<numsSize; i++)
+        {
+            if(currsum + nums[i] <= mid)
+                currsum += nums[i];
+            else
+            {   
+                if(subArray > k)
+                    break; 
+                subArray++;
+                currsum = nums[i];
+            }
+        }
+        if(subArray <= k)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
