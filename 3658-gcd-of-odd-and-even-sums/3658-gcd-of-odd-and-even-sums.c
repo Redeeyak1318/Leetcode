df7e7915ed6fdbbd79ce4adce2f07c2f950754d4
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-15 22:24:04 +0530

    Time: 0 ms (100%), Space: 9 MB (56.54%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.c
@@ -0,0 +1,11 @@
+int gcdOfOddEvenSums(int n) {
+    int sumOdd = n * n;
+    int sumEven = n * (n + 1);
+
+    while (sumEven != 0) {
+        int temp = sumEven;
+        sumEven = sumOdd % sumEven;
+        sumOdd = temp;
+    }
+    return sumOdd;
+}
