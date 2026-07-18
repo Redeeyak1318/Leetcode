b2f624640fec59bb14059488779ebf3df52fd454
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-18 22:56:54 +0530

    Time: 0 ms (100%), Space: 9.2 MB (23.55%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.c
@@ -0,0 +1,24 @@
+int findGCD(int* nums, int numsSize) {
+    int smallest = nums[0];
+    int largest = nums[0];
+    int gcd = 1;
+    for(int i=1; i<numsSize; i++)
+    {
+        if(nums[i] < smallest)
+            smallest = nums[i];
+        if(nums[i] > largest)
+            largest = nums[i];
+    }
+    
+    if(smallest == largest)
+        return smallest;
+    
+    for(int i=1; i <= smallest && i <= largest; ++i)
+    {
+        
+        if(smallest%i==0 && largest%i==0)
+            gcd = i;
+    }
+
+    return gcd;
+}
