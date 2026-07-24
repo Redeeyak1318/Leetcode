06d56ad3a9e1abcbebc0a733cd7c3242e3605e03
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-24 12:52:20 +0530

    Time: 2 ms (76.44%), Space: 19.4 MB (26.35%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        low = 0
+        high = n
+        while(low <= high):
+        
+            mid = low + (high-low) // 2
+            if(mid * (mid+1) // 2 <= n):
+            
+                low = mid+1
+            
+            else:
+                high = mid-1
+            
+        
+        return high
