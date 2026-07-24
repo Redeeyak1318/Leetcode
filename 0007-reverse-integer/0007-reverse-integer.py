6b495524d2db8742847a1790e1984878297b298d
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-07-24 14:28:41 +0530

    Time: 20 ms (42.17%), Space: 12.3 MB (87.07%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,5 +1,9 @@
-class Solution:
-    def reverse(self, x: int) -> int:
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
         sign = -1 if x < 0 else 1
         
        
