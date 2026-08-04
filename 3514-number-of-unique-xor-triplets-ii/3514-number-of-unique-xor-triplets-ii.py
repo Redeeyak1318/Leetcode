341215b06a977390b850b5c6856d633f5884f62f
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-08-04 13:45:07 +0530

    Time: 3121 ms (90.05%), Space: 19.6 MB (33.51%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        unique_nums = list(set(nums))
+        
+        possible_pairs = [False] * 2048
+        triplets = [False] * 2048
+        
+        for i in range(len(unique_nums)):
+            for j in range(i, len(unique_nums)):
+                possible_pairs[unique_nums[i] ^ unique_nums[j]] = True
+                
+        for pair_xor in range(2048):
+            if possible_pairs[pair_xor]:
+                for num in unique_nums:
+                    triplets[pair_xor ^ num] = True
+                    
+        return sum(triplets)
