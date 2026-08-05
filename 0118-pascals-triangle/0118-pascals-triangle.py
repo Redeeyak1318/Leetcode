07f1d9cf8ffa20336c49c135bc8440f4af84b65b
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-08-05 21:23:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.3%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
         triangle = []
         
         for i in range(numRows):
