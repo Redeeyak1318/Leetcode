f7cee1a96be749247feb87cb7c3486fb478e4eec
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-08-06 18:06:53 +0530

    Time: 70 ms (64.24%), Space: 121.3 MB (53.92%) - LeetHub

---
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rearrangeSticks(int n, int k) {
+        long long MOD = 1e9 + 7;
+        std::vector<long long> dp(k + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            std::vector<long long> next_dp(k + 1, 0);
+            for (int j = 1; j <= k && j <= i; j++) {
+                next_dp[j] = (dp[j - 1] + (i - 1) * dp[j]) % MOD;
+            }
+            dp = move(next_dp);
+        }
+
+        return dp[k];
+    }
+};
