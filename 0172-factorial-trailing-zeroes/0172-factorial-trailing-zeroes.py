e3613055dbcd2b48852363eb6c6fef801ee5dc1e
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-08-08 23:28:59 +0530

    Time: 0 ms (100%), Space: 12.2 MB (90.76%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,5 +1,9 @@
-class Solution:
-    def trailingZeroes(self, n: int) -> int:
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         count = 0
         while n > 0:
             n //= 5
