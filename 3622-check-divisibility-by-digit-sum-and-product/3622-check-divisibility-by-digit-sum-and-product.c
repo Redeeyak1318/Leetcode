35f90f2e2aaf37d763a372787fd08affdca0a6b5
Raktim Sonowal <216751300+Redeeyak1318@users.noreply.github.com>
2026-08-23 22:24:54 +0530

    Time: 0 ms (100%), Space: 8.4 MB (71.56%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.c
@@ -0,0 +1,22 @@
+bool checkDivisibility(int n) {
+    int ori=n;
+    int n1=ori;
+    int sum=0;
+    int prod=1;
+    int sum_prod;
+    while(n!=0)
+    {
+        sum += n%10;
+        n/=10;
+    }
+    while(ori!=0)
+    {
+        prod *= ori%10;
+            ori/=10;
+    }
+    sum_prod=sum+prod;
+    if(n1%sum_prod==0)
+        return true;
+    else
+        return false;
+}
